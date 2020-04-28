00b79f301b7ba26433477f86fe7ce78a6921e479
Mauricio Medvescig <mauricio.m@geopagos.com>
2020-04-28 17:11:28 -0300

    Swagger config

--- src/View/Product/ProductListView.php
+++ src/View/Product/ProductListView.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace Sylius\ShopApiPlugin\View\Product;
 
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Swagger\Annotations as SWG;
+
 class ProductListView
 {
-    /** @var ProductView[] */
+    /**
+     * @var ProductView[]
+     * @SWG\Property(
+     *     type="array",
+     *     @SWG\Items(ref=@Model(type=Sylius\ShopApiPlugin\View\Product\ProductView::class))
+     * )
+     */
     public $items = [];
 }
