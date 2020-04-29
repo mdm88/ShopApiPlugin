f7e1f79ecf466d3ebdbdae80716cee6970c46fc2
Mauricio Medvescig <mauricio.m@geopagos.com>
2020-04-29 11:24:36 -0300

    Swagger config

--- src/Controller/Product/AddReviewByCodeAction.php
+++ src/Controller/Product/AddReviewByCodeAction.php
@@ -74,6 +74,7 @@ final class AddReviewByCodeAction
      * @SWG\Response(
      *     response=400,
      *     description="Invalid input, validation failed."
+     *     @Model(type=Sylius\ShopApiPlugin\View\ValidationErrorView::class)
      * )
      *
      * @param Request $request
--- src/Controller/Product/AddReviewBySlugAction.php
+++ src/Controller/Product/AddReviewBySlugAction.php
@@ -74,6 +74,7 @@ final class AddReviewBySlugAction
      * @SWG\Response(
      *     response=400,
      *     description="Invalid input, validation failed."
+     *     @Model(type=Sylius\ShopApiPlugin\View\ValidationErrorView::class)
      * )
      *
      * @param Request $request
