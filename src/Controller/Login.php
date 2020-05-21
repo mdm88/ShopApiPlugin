3873e9246cb688e2b814cd5eac2858997a351e91
Mauricio Medvescig <mauricio.m@geopagos.com>
2020-05-21 15:27:48 -0300

    Swagger config

--- src/Controller/Login.php
+++ src/Controller/Login.php
@@ -4,22 +4,30 @@ declare(strict_types=1);
 
 namespace Sylius\ShopApiPlugin\Controller;
 
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Swagger\Annotations as SWG;
 
-/**
- * @SWG\Post(
- *     path="/shop-api/login",
- *     @SWG\Tag(name="Users")
- *     @SWG\Parameter(
- *         name="content",
- *         in="body",
- *         required=true,
- *         @Model(type=Sylius\ShopApiPlugin\Request\Customer\LoginRequest::class)
- *     )
- *     @SWG\Response(
- *         response="200",
- *         description="An example resource",
- *         @Model(type=Sylius\ShopApiPlugin\View\Customer\LoginSuccessView::class)
- *     )
- * )
- */
+final class Login
+{
+    /**
+     * @SWG\Post(
+     *     path="/shop-api/login",
+     *     @SWG\Tag(name="Users")
+     *     @SWG\Parameter(
+     *         name="content",
+     *         in="body",
+     *         required=true,
+     *         @Model(type=Sylius\ShopApiPlugin\Request\Customer\LoginRequest::class)
+     *     )
+     *     @SWG\Response(
+     *         response="200",
+     *         description="An example resource",
+     *         @Model(type=Sylius\ShopApiPlugin\View\Customer\LoginSuccessView::class)
+     *     )
+     * )
+     */
+    public function __invoke()
+    {
+
+    }
+}
