02f58cfe402ac5c03f4c567a622a00b2533ea8a8
Mauricio Medvescig <mauricio.m@geopagos.com>
2020-05-22 15:00:55 -0300

    Swagger config

--- src/Controller/Product/AddReviewBySlugAction.php
+++ src/Controller/Product/AddReviewBySlugAction.php
@@ -8,11 +8,13 @@ use FOS\RestBundle\View\View;
 use FOS\RestBundle\View\ViewHandlerInterface;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use Sylius\Component\Channel\Context\ChannelContextInterface;
+use Sylius\Component\Channel\Context\ChannelNotFoundException;
 use Sylius\Component\Core\Model\ChannelInterface;
 use Sylius\ShopApiPlugin\CommandProvider\ChannelBasedCommandProviderInterface;
 use Sylius\ShopApiPlugin\Factory\ValidationErrorViewFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Swagger\Annotations as SWG;
 
@@ -76,25 +78,36 @@ final class AddReviewBySlugAction
      *     description="Invalid input, validation failed.",
      *     @Model(type=Sylius\ShopApiPlugin\View\ValidationErrorView::class)
      * )
+     * @SWG\Response(
+     *     response=404,
+     *     description="Product not found.",
+     *     @Model(type=Sylius\ShopApiPlugin\View\ValidationErrorView::class)
+     * )
      *
      * @param Request $request
      * @return Response
      */
     public function __invoke(Request $request): Response
     {
-        /** @var ChannelInterface $channel */
-        $channel = $this->channelContext->getChannel();
+        try {
+            /** @var ChannelInterface $channel */
+            $channel = $this->channelContext->getChannel();
 
-        $validationResults = $this->addReviewBySlugCommandProvider->validate($request, $channel);
-        if (0 !== count($validationResults)) {
-            return $this->viewHandler->handle(View::create(
-                $this->validationErrorViewFactory->create($validationResults),
-                Response::HTTP_BAD_REQUEST
-            ));
+            $validationResults = $this->addReviewBySlugCommandProvider->validate($request, $channel);
+            if (0 !== count($validationResults)) {
+                return $this->viewHandler->handle(View::create(
+                    $this->validationErrorViewFactory->create($validationResults),
+                    Response::HTTP_BAD_REQUEST
+                ));
+            }
+
+            $this->bus->dispatch($this->addReviewBySlugCommandProvider->getCommand($request, $channel));
+
+            return $this->viewHandler->handle(View::create(null, Response::HTTP_CREATED));
+        } catch (ChannelNotFoundException $exception) {
+            throw new NotFoundHttpException('Channel has not been found.');
+        } catch (\InvalidArgumentException $exception) {
+            throw new NotFoundHttpException($exception->getMessage());
         }
-
-        $this->bus->dispatch($this->addReviewBySlugCommandProvider->getCommand($request, $channel));
-
-        return $this->viewHandler->handle(View::create(null, Response::HTTP_CREATED));
     }
 }
